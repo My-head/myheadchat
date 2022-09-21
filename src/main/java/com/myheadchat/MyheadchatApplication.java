3507c8f0853954f8345a92acad18308fa09e019d
My-head <mixedvlad@gmail.com>
2022-09-21 19:59:06 +0300

    Initial commit

---
+++ src/main/java/com/myheadchat/MyheadchatApplication.java
@@ -0,0 +1,13 @@
+package com.myheadchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyheadchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyheadchatApplication.class, args);
+	}
+
+}
