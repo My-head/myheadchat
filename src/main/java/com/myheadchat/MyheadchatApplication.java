9cffd422171cb568ddaca72af372622301bb0d82
My-head <mixedvlad@gmail.com>
2022-09-21 20:02:55 +0300

    start

--- src/main/java/com/myheadchat/MyheadchatApplication.java
+++ src/main/java/com/myheadchat/MyheadchatApplication.java
@@ -3,6 +3,9 @@ package com.myheadchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+///me
+
 @SpringBootApplication
 public class MyheadchatApplication {
 
